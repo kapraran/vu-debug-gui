7c6c85247a01715f121cee0ef11abb0e6eae8667
Nikos Kapraras <nikos@kapraran.dev>
2026-05-05 19:05:10 +0200

    migrate to vite

--- ui/src/app/enums/DebugGUIControlType.ts
+++ ui/src/app/enums/DebugGUIControlType.ts
@@ -1,13 +1,15 @@
-enum DebugGUIControlType {
-  Button = 1,
-  Checkbox,
-  Text,
-  Range,
-  Dropdown,
-  Number,
-  Vec2,
-  Vec3,
-  Vec4,
-}
+export const DebugGUIControlType = {
+  Button: 1,
+  Checkbox: 2,
+  Text: 3,
+  Number: 4,
+  Range: 5,
+  Dropdown: 6,
+  Vec2: 7,
+  Vec3: 8,
+  Vec4: 9,
+} as const;
+
+export type DebugGUIControlType = (typeof DebugGUIControlType)[keyof typeof DebugGUIControlType];
 
 export default DebugGUIControlType;
--- ui/src/app/enums/DebugGUICustomEvent.ts
+++ ui/src/app/enums/DebugGUICustomEvent.ts
@@ -1,6 +1,8 @@
-enum DebugGUICustomEvent {
-  UIEvent = "DBGUI:UIEvent",
-  ResetMKB = "DBGUI:ResetMKB",
-}
+export const DebugGUICustomEvent = {
+  UIEvent: "DBGUI:UIEvent",
+  ResetMKB: "DBGUI:ResetMKB",
+} as const;
+
+export type DebugGUICustomEvent = (typeof DebugGUICustomEvent)[keyof typeof DebugGUICustomEvent];
 
 export default DebugGUICustomEvent;
